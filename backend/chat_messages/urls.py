ecbdf235cb19fac6dab782b6be281b5eb0d53358
J0na555 <ethiopediaa@gmail.com>
2025-10-01 14:11:42 +0300

    feat: add GetMessages view and update urls for message retrieval by room

--- backend/chat_messages/urls.py
+++ backend/chat_messages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MessageListCreateView
+from .views import MessageListCreateView, GetMessages
 
 app_name = 'chat_messages'
 
 urlpatterns = [
     path('', MessageListCreateView.as_view(), name='messages'),
+    path('list/<int:room_id>/', GetMessages.as_view(), name='message-list'),
 ]
--- backend/chat_messages/views.py
+++ backend/chat_messages/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from .models import Message
+from rooms.models import Room
 from .serializers import MessageSerializer
 
 
@@ -9,11 +10,13 @@ class MessageListCreateView(generics.ListCreateAPIView):
     queryset = Message.objects.select_related('user', 'room')
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
-
-# class ChatDetail(generics.ListAPIView):
-#     serializer_class = MessageSerializer
-#     permission_classes = [IsAuthenticated]
+        
     
-#     def get_queryset(self):
-#         room_id = self.kwargs['room_id']
-#         return Message.objects.filter(room_id=room_id).select_related('user', 'room')
+
+class GetMessages(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        room_id = self.kwargs['room_id']
+        return Message.objects.filter(room_id=room_id)
